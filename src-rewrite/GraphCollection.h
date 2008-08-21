822e467510dae44032311284013c4cd2417af224
Tomaz Martins dos Santos Canabrava <tomaz.canabrava@gmail.com>
2008-08-21 15:54:56 +0000

    fixes the build of playground
    svn path=/trunk/playground/edu/Rocs/; revision=850472

---
+++ src-rewrite/GraphCollection.h
@@ -0,0 +1,52 @@
+/* This file is part of Rocs, 
+   Copyright (C) 2008 by:
+   Tomaz Canabrava <tomaz.canabrava@gmail.com>
+   Ugo Sangiori <ugorox@gmail.com>
+
+   Rocs is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Rocs is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Step; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef GRAPHCOLLECTION_H
+#define GRAPHCOLLECTION_H
+
+#include <QObject>
+#include <QList>
+#include <QString>
+
+class Graph;
+
+class GraphCollection : public QObject
+{
+Q_OBJECT
+
+public:
+  GraphCollection(const QString& name = "untitled");
+  void createNewGraph();
+  void removeGraph(int pos);
+  Graph* graphAt(int pos);
+
+  QString name() { return _name; }
+  void setName(const QString& name){ _name = name; }
+
+signals:
+void graphCreated(Graph *g);
+void graphRemoved(int pos);
+
+private:
+  QList<Graph*> _graphs;
+  QString _name;
+};
+
+#endif
