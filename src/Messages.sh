79fa5464d3a7851c5479b00151d91a37b0c53048
Pino Toscano <pino@kde.org>
2009-04-03 15:40:30 +0000

    lowercase the generated pot, like the appname
    svn path=/trunk/playground/edu/Rocs/; revision=948726

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` -o $podir/Rocs.pot
+$XGETTEXT `find . -name \*.cpp` -o $podir/rocs.pot
 rm -f rc.cpp
