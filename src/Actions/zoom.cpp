5c3983098d3899ae0a10681ced7cf66bd1b3b2bb
Andreas Cord-Landwehr <cola@uni-paderborn.de>
2011-05-27 23:05:33 +0200

    Fix licence issues in Action/*.
    Use licence headers from http://techbase.kde.org/Policies/Licensing_Policy.

--- src/Actions/zoom.cpp
+++ src/Actions/zoom.cpp
@@ -1,6 +1,6 @@
 /*
-    <one line to give the library's name and an idea of what it does.>
-    Copyright (C) 2011  <copyright holder> <email>
+    This file is part of Rocs.
+    Copyright 2011  Tomaz Canabrava <tomaz.canabrava@gmail.com>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
