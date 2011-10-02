ddd52254edb3aaebccd0eb3e3e185cd048d3e6bb
Andreas Cord-Landwehr <cola@uni-paderborn.de>
2011-10-02 19:33:53 +0200

    Change Action's name.

--- src/Interface/MainWindow.cpp
+++ src/Interface/MainWindow.cpp
@@ -307,7 +307,7 @@ void MainWindow::setupActions()
     _paletteActions->addAction ( "align-vcenter",new AlignAction ( i18n ( "Align on the center" ),AlignAction::VCenter,_graphVisualEditor ) );
     _paletteActions->addAction ( "align-vright", new AlignAction ( i18n ( "Align on the right" ), AlignAction::Right,  _graphVisualEditor ) );
     _paletteActions->addAction ( "align-circle", new AlignAction ( i18n ( "Align on a circle" ),  AlignAction::Circle,  _graphVisualEditor ) );
-    _paletteActions->addAction ( "align-tree", new AlignAction ( i18n ( "Align on a tree with minimal number of cuts." ),  AlignAction::MinCutTree,  _graphVisualEditor ) );
+    _paletteActions->addAction ( "align-tree", new AlignAction ( i18n ( "Minimize Crossing Edges" ),  AlignAction::MinCutTree,  _graphVisualEditor ) );
 
     createAction("document-new",     i18n("New Graph"),         "new-graph",         Qt::Key_N, SLOT(newGraph()),    this);
     createAction("document-open",    i18n("Open Graph"),        "open-graph",        Qt::Key_0, SLOT(openGraph()),   this);
