108090dfa7948bde640018974ea98a022c4de495
Andreas Cord-Landwehr <cola@uni-paderborn.de>
2011-10-09 10:31:28 +0200

    Fixed missing copyright tags from history.
    Now all copyright issues should be fixed.
    
    BUG: 277401

--- src/Tests/TestDataStructure.cpp
+++ src/Tests/TestDataStructure.cpp
@@ -1,6 +1,6 @@
 /*
-    A Roc's test Case
-    Copyright (C) 2010  Wagner Reck <wagner.reck@gmail.com>
+    This file is part of Rocs.
+    Copyright 2010       Wagner Reck <wagner.reck@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
