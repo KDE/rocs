1274241bd89b86293ba3efc35179bc9e6d189d60
Andreas Cord-Landwehr <cola@uni-paderborn.de>
2012-02-02 19:40:56 +0100

    Fix license issues to be compatible to KDE licensing policy.
    Relicensing and additional licenses were discussed by all
    copyright owners at recent IRC meeting.

--- src/Core/DocumentManager.cpp
+++ src/Core/DocumentManager.cpp
@@ -4,10 +4,10 @@
     Copyright 2010-2011  Wagner Reck <wagner.reck@gmail.com>
     Copyright 2011       Andreas Cord-Landwehr <cola@uni-paderborn.de>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of 
+    the License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
--- src/Tests/TestDynamicProperties.cpp
+++ src/Tests/TestDynamicProperties.cpp
@@ -3,10 +3,10 @@
     Copyright 2010       Tomaz Canabrava <tomaz.canabrava@gmail.com>
     Copyright 2010       Wagner Reck <wagner.reck@gmail.com>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of 
+    the License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,7 +15,6 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 */
 
 #include "TestDynamicProperties.h"
