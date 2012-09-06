e1c671d55e1470e4bd578063d4286e99edd0342c
Andreas Cord-Landwehr <cola@uni-paderborn.de>
2012-09-06 21:16:35 +0200

    Add *.gv file ending as type for Graphvis files.

--- src/LoadSave/Plugins/dotFileFormat/DotFileFormatPlugin.cpp
+++ src/LoadSave/Plugins/dotFileFormat/DotFileFormatPlugin.cpp
@@ -58,7 +58,7 @@ DotFileFormatPlugin::DotFileFormatPlugin(QObject* parent, const QList< QVariant 
 const QStringList DotFileFormatPlugin::extensions() const
 {
     return QStringList()
-           << i18n("*.dot|Graphviz Format") + '\n';
+           << i18n("*.dot *.gv|Graphviz Format") + '\n';
 }
 
 
