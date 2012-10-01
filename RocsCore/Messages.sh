a39fb5d4d7e8fef8e6f658253bb27baa149d65f2
Andreas Cord-Landwehr <cola@uni-paderborn.de>
2012-10-01 13:05:04 +0200

    Fix cataloge creation.

--- RocsCore/Messages.sh
+++ RocsCore/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` -o $podir/rocs.pot
+$XGETTEXT `find . -name \*.cpp` -o $podir/rocscore.pot
 rm -f rc.cpp
