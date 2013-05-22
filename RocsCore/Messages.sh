1d55c556591abd63fc88bfc4459f82e0263bc955
Andreas Cord-Landwehr <cola@uni-paderborn.de>
2013-05-22 07:59:31 +0200

    Fix message extraction.

--- RocsCore/Messages.sh
+++ RocsCore/Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-extractrc --context="Scripting API Description" --tag-group=none --tag=para `find EngineModules/* DataStructures/* * -iname '*.xml'` >> rc.cpp
+$EXTRACTRC --context="Scripting API Description" --tag-group=none --tag=para `find EngineModules/* DataStructures/* * -iname '*.xml'` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp` -o $podir/rocscore.pot
 rm -f rc.cpp
