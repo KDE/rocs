9626e24bc67fce611c7c4eda94582f864278465b
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2014-04-05 13:14:15 +0200

    Create NodeType class.

---
+++ libgraphtheory/nodetype.h
@@ -0,0 +1,63 @@
+/*
+ *  Copyright 2014  Andreas Cord-Landwehr <cordlandwehr@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) version 3, or any
+ *  later version accepted by the membership of KDE e.V. (or its
+ *  successor approved by the membership of KDE e.V.), which shall
+ *  act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NODETYPE_H
+#define NODETYPE_H
+
+#include "libgraphtheoryexport.h"
+
+#include <QObject>
+#include <QSharedPointer>
+
+namespace GraphTheory
+{
+
+class NodeType;
+class NodeTypePrivate;
+typedef QSharedPointer<NodeType> NodeTypePtr;
+
+/**
+ * \class Node
+ */
+class GRAPHTHEORY_EXPORT NodeType : public QObject
+{
+    Q_OBJECT
+
+public:
+    /**
+     * Creates a new NodeType.
+     *
+     * @return a NodeType object
+     */
+    static NodeTypePtr create();
+
+    /** Destroys the edge type */
+    virtual ~NodeType();
+
+protected:
+    NodeType();
+
+private:
+    Q_DISABLE_COPY(NodeType)
+    const QScopedPointer<NodeTypePrivate> d;
+};
+}
+
+#endif
