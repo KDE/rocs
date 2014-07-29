5517efea00238f38e4d356abad20083a1d20a280
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2014-07-29 22:51:40 +0200

    Port value assigner.

---
+++ libgraphtheory/modifiers/valueassign.cpp
@@ -0,0 +1,147 @@
+/*
+ *  Copyright 2011-2014  Andreas Cord-Landwehr <cordlandwehr@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) version 3, or any
+ *  later version accepted by the membership of KDE e.V. (or its
+ *  successor approved by the membership of KDE e.V.), which shall
+ *  act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "valueassign.h"
+#include "node.h"
+#include "edge.h"
+#include <limits.h>
+#include <QString>
+#include <QVariant>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_int.hpp>
+#include <boost/random/uniform_real.hpp>
+#include <boost/random/variate_generator.hpp>
+
+using namespace GraphTheory;
+
+ValueAssign::ValueAssign()
+{
+}
+
+
+template<typename T>
+void ValueAssign::enumerate(const QList<T> &list, const QString &property, int start, const QString &baseString, bool overrideValues)
+{
+    for (int i = 0; i < list.size(); i++) {
+        if (!overrideValues && !list[i]->dynamicProperty(property).isNull()) {
+            return;
+        }
+        list[i]->setDynamicProperty(property, baseString + QString::number(start++));
+    }
+}
+template GRAPHTHEORY_EXPORT void ValueAssign::enumerate<NodePtr>(const QList<NodePtr> &list, const QString &property, int start, const QString &baseString, bool overrideValues);
+template GRAPHTHEORY_EXPORT void ValueAssign::enumerate<EdgePtr>(const QList<EdgePtr> &list, const QString &property, int start, const QString &baseString, bool overrideValues);
+
+
+template<typename T>
+void ValueAssign::enumerateAlpha(const QList< T >& list, const QString &property, const QString &start, bool overrideValues)
+{
+    QString identifier = start;
+    for (int i = start.length()-1; i >= 0; --i) {
+        // ensure that we only have letters
+        if (!identifier.at(i).isLetter()) {
+            identifier.replace(i, 1, 'a');
+        }
+    }
+
+    for (int i = 0; i < list.size(); i++) {
+        if (!overrideValues && !list[i]->dynamicProperty(property).isNull()) {
+            return;
+        }
+        list[i]->setDynamicProperty(property, identifier);
+
+        // compute new identifier by lexicographical increasing
+        for (int i = identifier.length()-1; i >= 0; --i) {
+            // ensure that we only have letters
+            if (identifier.at(i) != 'Z') {
+                identifier.replace(i, 1, QChar(identifier.at(i).toLatin1() + 1));
+                break; // we are done, do not loop further
+            } else {
+                identifier.replace(i, 1, 'a');
+            }
+            if (i == 0 && identifier.at(0) == 'a') {
+                identifier.append('a');
+            }
+        }
+    }
+}
+template GRAPHTHEORY_EXPORT void ValueAssign::enumerateAlpha<NodePtr>(const QList<NodePtr> &list, const QString &property, const QString &start, bool overrideValues);
+template GRAPHTHEORY_EXPORT void ValueAssign::enumerateAlpha<EdgePtr>(const QList<EdgePtr> &list, const QString &property, const QString &start, bool overrideValues);
+
+
+template<typename T>
+void ValueAssign::assignRandomIntegers(const QList<T> &list, const QString &property, int lowerLimit, int upperLimit, int seed, bool overrideValues)
+{
+    if (lowerLimit > upperLimit) {
+        return;
+    }
+
+    boost::mt19937 gen;
+    gen.seed(static_cast<unsigned int>(seed));
+
+    boost::uniform_int<> distribution(lowerLimit, upperLimit);
+    boost::variate_generator<boost::mt19937&, boost::uniform_int<> > die(gen, distribution);
+
+    for (int i = 0; i < list.size(); i++) {
+        if (!overrideValues && !list[i]->dynamicProperty(property).isNull()) {
+            return;
+        }
+        list[i]->setDynamicProperty(property, QString::number(die()));
+    }
+}
+template GRAPHTHEORY_EXPORT void ValueAssign::assignRandomIntegers<NodePtr>(const QList<NodePtr> &list, const QString &property, int lowerLimit, int upperLimit, int seed, bool overrideValues);
+template GRAPHTHEORY_EXPORT void ValueAssign::assignRandomIntegers<EdgePtr>(const QList<EdgePtr> &list, const QString &property, int lowerLimit, int upperLimit, int seed, bool overrideValues);
+
+
+template<typename T>
+void ValueAssign::assignRandomReals(const QList<T> &list, const QString &property, qreal lowerLimit, qreal upperLimit, int seed, bool overrideValues)
+{
+    if (lowerLimit > upperLimit) {
+        return;
+    }
+
+    boost::mt19937 gen;
+    gen.seed(static_cast<unsigned int>(seed));
+
+    boost::uniform_real<> distribution(lowerLimit, upperLimit);
+    boost::variate_generator<boost::mt19937&, boost::uniform_real<> > die(gen, distribution);
+
+    for (int i = 0; i < list.size(); i++) {
+        if (!overrideValues && !list[i]->dynamicProperty(property).isNull()) {
+            return;
+        }
+        list[i]->setDynamicProperty(property, QString::number(die()));
+    }
+}
+template GRAPHTHEORY_EXPORT void ValueAssign::assignRandomReals<NodePtr>(const QList<NodePtr> &list, const QString &property, qreal lowerLimit, qreal upperLimit, int seed, bool overrideValues);
+template GRAPHTHEORY_EXPORT void ValueAssign::assignRandomReals<EdgePtr>(const QList<EdgePtr> &list, const QString &property, qreal lowerLimit, qreal upperLimit, int seed, bool overrideValues);
+
+template<typename T>
+void ValueAssign::assignConstantValue(const QList<T> &list, const QString &property, const QString &constant, bool overrideValues)
+{
+    for (int i = 0; i < list.size(); i++) {
+        if (!overrideValues && !list[i]->dynamicProperty(property).isNull()) {
+            return;
+        }
+        list[i]->setDynamicProperty(property, constant);
+    }
+}
+template GRAPHTHEORY_EXPORT void ValueAssign::assignConstantValue<NodePtr>(const QList<NodePtr> &list, const QString &property, const QString &constant, bool overrideValues);
+template GRAPHTHEORY_EXPORT void ValueAssign::assignConstantValue<EdgePtr>(const QList<EdgePtr> &list, const QString &property, const QString &constant, bool overrideValues);
---
+++ libgraphtheory/modifiers/valueassign.h
@@ -0,0 +1,101 @@
+/*
+ *  Copyright 2011-2014  Andreas Cord-Landwehr <cordlandwehr@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) version 3, or any
+ *  later version accepted by the membership of KDE e.V. (or its
+ *  successor approved by the membership of KDE e.V.), which shall
+ *  act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef VALUEASSIGN_H
+#define VALUEASSIGN_H
+
+#include "typenames.h"
+#include "libgraphtheoryexport.h"
+
+namespace GraphTheory
+{
+
+/** \brief this class provides value assigner
+ */
+class GRAPHTHEORY_EXPORT ValueAssign
+{
+
+public:
+    ValueAssign();
+    /**
+     * Assign integers in increasing order starting at 'start' to all nodes.
+     *
+     * \param list QList of EdgePtr or NodePtr
+     * \param property the property the shall be set to specified value
+     * \param start the integer to start from
+     * \param baseString the string to append the successive integers to
+     * \param overrideValues if true, current property values are overwritten; otherwise not
+     */
+    template<typename T>
+    void enumerate(const QList<T>& list, const QString &property, int start, const QString &baseString, bool overrideValues = true);
+
+    /**
+     * Assign strings in increasing order starting at 'start' to all nodes.
+     *
+     * \param list QList of EdgePtr or NodePtr
+     * \param property the property the shall be set to specified value
+     * \param start the string to start from
+     * \param overrideValues if true, current property values are overwritten; otherwise not
+     */
+    template<typename T>
+    void enumerateAlpha(const QList<T> &list, const QString &property, const QString &start, bool overrideValues = true);
+
+    /**
+     * Assign integers uniformly at random from range [lowerLimit,upperLimit] to data elements. Using Mersenne-Twister
+     * random number generator, initialized by 'seed'. If not lowerLimit < upperLimit the function returns without
+     * any operation.
+     *
+     * \param list QList of EdgePtr or NodePtr
+     * \param property the property the shall be set to specified value
+     * \param lowerLimit the lower limit for random number interval
+     * \param upperLimit the upper limit for random number interval
+     * \param overrideValues if true, current property values are overwritten; otherwise not
+     */
+    template<typename T>
+    void assignRandomIntegers(const QList<T> &list, const QString &property, int lowerLimit, int upperLimit, int seed, bool overrideValues = true);
+
+    /**
+     * Assign float values uniformly at random from range [lowerLimit,upperLimit] to nodes. Using Mersenne-Twister
+     * random number generator, initialized by 'seed'. If not lowerLimit < upperLimit the function returns without
+     * any operation.
+     *
+     * \param list QList of EdgePtr or NodePtr
+     * \param property the property the shall be set to specified value
+     * \param lowerLimit the lower limit for random number interval
+     * \param upperLimit the upper limit for random number interval
+     * \param overrideValues if true, current property values are overwritten; otherwise not
+     */
+    template<typename T>
+    void assignRandomReals(const QList<T> &list, const QString &property, qreal lowerLimit, qreal upperLimit, int seed, bool overrideValues = true);
+
+    /**
+     * Assign a constant string value to nodes/pointers
+     *
+     * \param list QList of EdgePtr or NodePtr
+     * \param property the property the shall be set to specified value
+     * \param constant a string to assign
+     * \param overrideValues if true, current property values are overwritten; otherwise not
+     */
+    template<typename T>
+    void assignConstantValue(const QList<T> &list, const QString &property, const QString &constant, bool overrideValues = true);
+};
+}
+
+#endif
