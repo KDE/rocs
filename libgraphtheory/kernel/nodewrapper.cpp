ed1703dfe9795432441a96eacc365e68ce27ce3e
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2014-08-09 11:13:29 +0200

    Node wrapper for use in simulation kernel.

---
+++ libgraphtheory/kernel/nodewrapper.cpp
@@ -0,0 +1,111 @@
+/*
+ *  Copyright 2014  Andreas Cord-Landwehr <cordlandwehr@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) version 3, or any
+ *  later version accepted by the membership of KDE e.V. (or its
+ *  successor approved by the membership of KDE e.V.), which shall
+ *  act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "nodewrapper.h"
+#include "graphdocument.h"
+#include "nodetype.h"
+#include "edge.h"
+
+#include <QPointF>
+#include <QColor>
+#include <QDebug>
+
+using namespace GraphTheory;
+
+NodeWrapper::NodeWrapper(NodePtr node)
+    : m_node(node)
+{
+    connect(m_node.data(), SIGNAL(colorChanged(QColor)), this, SIGNAL(colorChanged(QColor)));
+    connect(m_node.data(), SIGNAL(positionChanged(QPointF)), this, SIGNAL(positionChanged(QPointF)));
+    connect(m_node.data(), SIGNAL(dynamicPropertiesChanged()), this, SLOT(updateDynamicProperties()));
+
+    updateDynamicProperties();
+}
+
+NodeWrapper::~NodeWrapper()
+{
+
+}
+
+int NodeWrapper::id() const
+{
+    return m_node->id();
+}
+
+qreal NodeWrapper::x() const
+{
+    return m_node->x();
+}
+
+void NodeWrapper::setX(qreal x)
+{
+    if (x == NodeWrapper::x()) {
+        return;
+    }
+    m_node->setX(x);
+    // signal emitted by connection to m_node signal
+}
+
+qreal NodeWrapper::y() const
+{
+    return m_node->y();
+}
+
+void NodeWrapper::setY(qreal y)
+{
+    if (y == NodeWrapper::y()) {
+        return;
+    }
+    m_node->setY(y);
+    // signal emitted by connection to m_node signal
+}
+
+QColor NodeWrapper::color() const
+{
+    return m_node->color();
+}
+
+void NodeWrapper::setColor(const QColor &color)
+{
+    if (color == m_node->color()) {
+        return;
+    }
+    m_node->setColor(color);
+    // signal emitted by connection to m_node signal
+}
+
+bool NodeWrapper::event(QEvent* e)
+{
+    if (e->type() == QEvent::DynamicPropertyChange) {
+        QDynamicPropertyChangeEvent *propertyEvent = static_cast<QDynamicPropertyChangeEvent *>(e);
+        QString name = QString::fromUtf8(propertyEvent->propertyName());
+        QVariant value = property(propertyEvent->propertyName());
+        m_node->setDynamicProperty(name, value);
+    }
+    return parent()->event(e);
+}
+
+void NodeWrapper::updateDynamicProperties()
+{
+    // set dynamic properties
+    foreach (QString property, m_node->dynamicProperties()) {
+        setProperty(property.toUtf8(), m_node->dynamicProperty(property));
+    }
+}
---
+++ libgraphtheory/kernel/nodewrapper.h
@@ -0,0 +1,108 @@
+/*
+ *  Copyright 2014  Andreas Cord-Landwehr <cordlandwehr@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) version 3, or any
+ *  later version accepted by the membership of KDE e.V. (or its
+ *  successor approved by the membership of KDE e.V.), which shall
+ *  act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NODEWRAPPER_H
+#define NODEWRAPPER_H
+
+#include "libgraphtheoryexport.h"
+#include "typenames.h"
+#include "node.h"
+#include "graphdocument.h"
+
+#include <QtScript>
+#include <QObject>
+#include <QColor>
+
+namespace GraphTheory
+{
+class NodeWrapperPrivate;
+
+/**
+ * \class NodeWrapper
+ * Wraps NodePtr to be accessible via QtScript. All properties of the node object are available
+ * as commong QObject properties.
+ */
+class NodeWrapper : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(int id READ id)
+    Q_PROPERTY(qreal x READ x WRITE setX NOTIFY positionChanged)
+    Q_PROPERTY(qreal y READ y WRITE setY NOTIFY positionChanged)
+    Q_PROPERTY(QColor color READ color WRITE setColor NOTIFY colorChanged)
+
+public:
+    NodeWrapper(NodePtr node);
+
+    virtual ~NodeWrapper();
+
+    /**
+     * If the id value is invalid, -1 is returned.
+     *
+     * @return node identifier
+     */
+    int id() const;
+
+    /**
+     * @return x-position of node
+     */
+    qreal x() const;
+
+    /**
+     * set x-position of node to @c x
+     */
+    void setX(qreal x);
+
+    /**
+     * @return y-position of node
+     */
+    qreal y() const;
+
+    /**
+     * set y-position of node to @c y
+     */
+    void setY(qreal y);
+
+    /**
+     * @return color of node
+     */
+    QColor color() const;
+
+    /**
+     * set color of node to @c color
+     */
+    void setColor(const QColor &color);
+
+    /** reimplemented from QObject **/
+    virtual bool event(QEvent* e);
+
+public Q_SLOTS:
+    void updateDynamicProperties();
+
+Q_SIGNALS:
+    void positionChanged(const QPointF &position);
+    void colorChanged(const QColor &color);
+
+private:
+    Q_DISABLE_COPY(NodeWrapper)
+    const NodePtr m_node;
+};
+}
+
+#endif
