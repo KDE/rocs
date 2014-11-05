43a66fa1996de9f2f7ca810a674cb13aa259bcd3
Burkhard Lück <lueck@hube-lueck.de>
2014-11-05 09:59:10 +0100

    extract messages from qml files as well

--- libgraphtheory/Messages.sh
+++ libgraphtheory/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` -o $podir/libgraphtheory.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.qml` -o $podir/libgraphtheory.pot
 rm -f rc.cpp
