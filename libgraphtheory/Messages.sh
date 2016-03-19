cf7b8c51c4118965eddbc93e325b152418bce8c6
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2016-03-19 17:47:37 +0100

    Fix API localization.
    BUG: 360116

--- libgraphtheory/Messages.sh
+++ libgraphtheory/Messages.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp
+$EXTRACTRC --context="Scripting API" --tag-group=none --tag=para --tag=info `find kernel/modules/* -iname '*.xml'` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp -o -name \*.qml` -o $podir/libgraphtheory.pot
 rm -f rc.cpp
