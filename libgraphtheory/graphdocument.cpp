8c6f0eac73b0fa63a8ee56c1acb091ac7167e2f9
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2019-06-23 23:30:24 -0300

    Change the default edges to something more verbose to the user

--- libgraphtheory/graphdocument.cpp
+++ libgraphtheory/graphdocument.cpp
@@ -127,8 +127,17 @@ GraphDocumentPtr GraphDocument::create()
     GraphDocumentPtr pi(new GraphDocument);
     pi->setQpointer(pi);
 
-    // create default type
-    EdgeType::create(pi)->setName(i18n("default"));
+    // create default edge bidirectional
+    EdgeTypePtr edgeTypeBidirectional = EdgeType::create(pi);
+    edgeTypeBidirectional->setName(i18n("bidirectional"));
+    edgeTypeBidirectional->setDirection(EdgeType::Direction::Bidirectional);
+
+    // create default edge unidirectional
+    EdgeTypePtr edgeTypeUnidirectional = EdgeType::create(pi);
+    edgeTypeUnidirectional->setName(i18n("unidirectional"));
+    edgeTypeUnidirectional->setDirection(EdgeType::Direction::Unidirectional);
+
+    // create default node type
     NodeType::create(pi)->setName(i18n("default"));
     pi->d->m_valid = true;
     pi->d->m_modified = false;
