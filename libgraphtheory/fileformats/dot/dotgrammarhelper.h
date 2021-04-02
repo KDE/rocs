65a371f405e12861cb694b20d8bb12edc086170e
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-04-02 12:41:57 +0200

    Manually convert remaining headers

--- libgraphtheory/fileformats/dot/dotgrammarhelper.h
+++ libgraphtheory/fileformats/dot/dotgrammarhelper.h
@@ -2,20 +2,7 @@
     This file is part of Rocs.
     SPDX-FileCopyrightText: 2006-2007 Gael de Chalendar <kleag@free.fr>
     SPDX-FileCopyrightText: 2012-2014 Andreas Cord-Landwehr <cordlandwehr@kde.org>
-
-    KGraphViewer is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public
-    License as published by the Free Software Foundation, version 2.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-    02110-1301, USA
+    SPDX-License-Identifier: GPL-2.0-only
 */
 
 #ifndef DOT_GRAMMARHELPER_H
