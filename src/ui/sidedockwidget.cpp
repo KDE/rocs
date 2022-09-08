230bdea774ad34fe02512974afc8ca54bdbe4836
Gabriel Nunes <gabrielbpn0802@gmail.com>
2022-09-08 11:23:51 -0300

    adjusting spacing with clang-format

--- src/ui/sidedockwidget.cpp
+++ src/ui/sidedockwidget.cpp
@@ -1,187 +1,172 @@
 /*
- *  SPDX-FileCopyrightText: 2012-2014 Andreas Cord-Landwehr <cordlandwehr@kde.org>
- *  SPDX-FileCopyrightText: 2019 Tomaz Canabrava <tcanabrava@kde.org>
+ *  SPDX-FileCopyrightText: 2012-2014 Andreas Cord-Landwehr
+ * <cordlandwehr@kde.org> SPDX-FileCopyrightText: 2019 Tomaz Canabrava
+ * <tcanabrava@kde.org>
  *
- *  SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+ *  SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR
+ * LicenseRef-KDE-Accepted-GPL
  */
 
 #include "sidedockwidget.h"
 
 #include <KAcceleratorManager>
+#include <KLocalizedString>
+#include <QAction>
+#include <QDebug>
+#include <QIcon>
+#include <QLabel>
+#include <QLayout>
+#include <QObject>
+#include <QStackedLayout>
 #include <QStyle>
 #include <QStyleOptionToolButton>
 #include <QStylePainter>
 #include <QToolBar>
-#include <QLabel>
-#include <QAction>
-#include <QStackedLayout>
-#include <QLayout>
-#include <QObject>
 #include <QWidget>
-#include <QIcon>
-#include <KLocalizedString>
-#include <QDebug>
 
+SideToolButton::SideToolButton(QWidget *parent) : QToolButton(parent) {
+  setFocusPolicy(Qt::NoFocus);
+  KAcceleratorManager::setNoAccel(this);
+  setCheckable(true);
+  setAutoRaise(true);
+  setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
 
-SideToolButton::SideToolButton(QWidget *parent)
-    : QToolButton(parent)
-{
-    setFocusPolicy(Qt::NoFocus);
-    KAcceleratorManager::setNoAccel(this);
-    setCheckable(true);
-    setAutoRaise(true);
-    setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
-
-    wasChecked = false;
+  wasChecked = false;
 }
 
-Qt::Orientation SideToolButton::orientation() const
-{
-    return Qt::Vertical;
+Qt::Orientation SideToolButton::orientation() const { return Qt::Vertical; }
+
+QSize SideToolButton::sizeHint() const {
+  ensurePolished();
+
+  QStyleOptionToolButton option;
+  initStyleOption(&option);
+
+  QFontMetrics fm = fontMetrics();
+
+  // calculate example character width
+  const int charWidth = fm.boundingRect(QLatin1Char('x')).width();
+
+  // compute text size
+  QSize textSize = fm.size(Qt::TextShowMnemonic, option.text);
+  textSize.rwidth() += 2 * charWidth;
+
+  // compute icon size
+  const int spacing = 2;
+  const int iconwidth = option.iconSize.height();
+  const int iconheight = option.iconSize.width();
+
+  // compute final size. height and width are rotated.
+  return {4 + qMax(textSize.height(), iconheight) + spacing,
+          4 + textSize.width() + iconwidth + spacing};
 }
 
-QSize SideToolButton::sizeHint() const
-{
-    ensurePolished();
+void SideToolButton::paintEvent(QPaintEvent *event) {
+  Q_UNUSED(event)
 
-    QStyleOptionToolButton option;
-    initStyleOption(&option);
+  // rotated paint
+  QStylePainter painter(this);
+  QStyleOptionToolButton option;
+  initStyleOption(&option);
 
-    QFontMetrics fm = fontMetrics();
+  // first draw normal frame and not text/icon
+  option.text.clear();
+  option.icon = QIcon();
+  painter.drawComplexControl(QStyle::CC_ToolButton, option);
 
-    // calculate example character width
-    const int charWidth = fm.boundingRect(QLatin1Char('x')).width();
+  // rotate the options
+  QSize size(option.rect.size());
+  size.transpose();
+  option.rect.setSize(size);
 
-    // compute text size
-    QSize textSize = fm.size(Qt::TextShowMnemonic, option.text);
-    textSize.rwidth() += 2 * charWidth;
+  // rotate the painter
+  painter.translate(width(), 0);
+  painter.rotate(90);
 
-    // compute icon size
-    const int spacing = 2;
-    const int iconwidth = option.iconSize.height();
-    const int iconheight = option.iconSize.width();
-
-    // compute final size. height and width are rotated.
-    return {
-        4 + qMax(textSize.height(), iconheight) + spacing,
-        4 + textSize.width() + iconwidth + spacing
-    };
+  // paint text and icon
+  option.text = text();
+  QIcon::Mode iconMode =
+      (option.state & QStyle::State_MouseOver) ? QIcon::Active : QIcon::Normal;
+  QPixmap ic = icon().pixmap(option.iconSize, iconMode, QIcon::On);
+  QTransform transform;
+  transform = transform.rotate(-90); // rotate right
+  option.icon = ic.transformed(transform, Qt::SmoothTransformation);
+  painter.drawControl(QStyle::CE_ToolButtonLabel, option);
+  painter.end();
 }
 
-void SideToolButton::paintEvent(QPaintEvent *event)
-{
-    Q_UNUSED(event)
+SidedockWidget::SidedockWidget(QWidget *parent)
+    : QWidget(parent), _btnGroup(new QButtonGroup(this)), _showDock(false) {
+  Q_ASSERT(parent); // we need a parent widget
 
-    // rotated paint
-    QStylePainter painter(this);
-    QStyleOptionToolButton option;
-    initStyleOption(&option);
+  _btnGroup->setExclusive(true);
 
-    // first draw normal frame and not text/icon
-    option.text.clear();
-    option.icon = QIcon();
-    painter.drawComplexControl(QStyle::CC_ToolButton, option);
-
-    // rotate the options
-    QSize size(option.rect.size());
-    size.transpose();
-    option.rect.setSize(size);
-
-    // rotate the painter
-    painter.translate(width(), 0);
-    painter.rotate(90);
-
-    // paint text and icon
-    option.text = text();
-    QIcon::Mode iconMode = (option.state & QStyle::State_MouseOver) ? QIcon::Active : QIcon::Normal;
-    QPixmap ic = icon().pixmap(option.iconSize, iconMode, QIcon::On);
-    QTransform transform;
-    transform = transform.rotate(-90); // rotate right
-    option.icon = ic.transformed(transform, Qt::SmoothTransformation);
-    painter.drawControl(QStyle::CE_ToolButtonLabel, option);
-    painter.end();
+  _toolBar = new QToolBar(i18nc("@title", "Side Toolbar"), this);
+  _toolBar->setFloatable(false);
+  _toolBar->setMovable(false);
+  _toolBar->setObjectName("sidebar");
+  _toolBar->setIconSize(QSize(16, 16));
+  _toolBar->setContextMenuPolicy(Qt::PreventContextMenu);
+  setLayout(new QStackedLayout);
 }
 
-SidedockWidget::SidedockWidget(QWidget* parent)
-    : QWidget(parent)
-    , _btnGroup(new QButtonGroup(this))
-    , _showDock(false)
-{
-    Q_ASSERT(parent); // we need a parent widget
+void SidedockWidget::addDock(QWidget *widget, const QString &title,
+                             const QIcon &icon) {
+  widget->setVisible(false);
+  layout()->addWidget(widget);
 
-    _btnGroup->setExclusive(true);
+  SideToolButton *button = new SideToolButton(this);
 
-    _toolBar = new QToolBar(i18nc("@title", "Side Toolbar"), this);
-    _toolBar->setFloatable(false);
-    _toolBar->setMovable(false);
-    _toolBar->setObjectName("sidebar");
-    _toolBar->setIconSize(QSize(16,16));
-    _toolBar->setContextMenuPolicy(Qt::PreventContextMenu);
-    setLayout(new QStackedLayout);
-}
+  button->setText(title);
+  button->setToolTip(i18n("Toggle '%1' view.", title));
+  button->setIcon(icon);
+  button->setShortcut(QKeySequence());
+  button->setChecked(false); // initially do not check
+  button->wasChecked = false;
+  _btnGroup->addButton(button);
 
-void SidedockWidget::addDock(QWidget* widget, const QString& title, const QIcon& icon)
-{
-    widget->setVisible(false);
-    layout()->addWidget(widget);
-    
-    SideToolButton* button = new SideToolButton(this);
+  // only request action on user set action
+  const int idx = _widgets.count();
+  int idx_aux = idx;
 
-    button->setText(title);
-    button->setToolTip(i18n("Toggle '%1' view.", title));
-    button->setIcon(icon);
-    button->setShortcut(QKeySequence());
-    button->setChecked(false); // initially do not check
-    button->wasChecked = false;
-    _btnGroup->addButton(button);
+  if (idx == 0) { // set widget of ElementTypes as standard
+    button->setChecked(true);
+    button->wasChecked = true;
+  }
 
-    // only request action on user set action
-    const int idx = _widgets.count();
-    int idx_aux = idx;
-
-    if (idx == 0) { // set widget of ElementTypes as standard
-	button->setChecked(true);
-        button->wasChecked = true;
-    }
-    
-    connect(button, &SideToolButton::clicked, this,
-        [this, button, idx, &idx_aux] {
-      
-      	// Force uncheck the button because of the exclusive
-        // Without this the toolbar buttons cannot be all unchecked
-        // after one is clicked
-	if (button->wasChecked) {	
-	    if (idx_aux == idx) {
+  connect(button, &SideToolButton::clicked, this,
+          [this, button, idx, &idx_aux] {
+            // Force uncheck the button because of the exclusive
+            // Without this the toolbar buttons cannot be all unchecked
+            // after one is clicked
+            if (button->wasChecked) {
+              if (idx_aux == idx) {
                 _btnGroup->setExclusive(false);
-            	button->setChecked(false);
-           	_btnGroup->setExclusive(true);
-           	button->wasChecked = false;
-	    } else {
-	          button->wasChecked = true;
-	      }
-	} else {
-            button->wasChecked = true;
-        }
-	
-	idx_aux = idx;
-        showDock(button->isChecked(), idx);
-    });
+                button->setChecked(false);
+                _btnGroup->setExclusive(true);
+                button->wasChecked = false;
+              } else {
+                button->wasChecked = true;
+              }
+            } else {
+              button->wasChecked = true;
+            }
 
-    // register and add to list
-    _toolBar->addWidget(button);
-    _widgets.append(widget);
+            idx_aux = idx;
+            showDock(button->isChecked(), idx);
+          });
+
+  // register and add to list
+  _toolBar->addWidget(button);
+  _widgets.append(widget);
 }
 
-void SidedockWidget::showDock(bool show, int nr)
-{
-    _showDock = show;
-    setVisible(show);
-    auto *stackLayout = qobject_cast<QStackedLayout*>(layout());
-    stackLayout->setCurrentIndex(nr);
-    Q_EMIT visibilityChanged(show);
+void SidedockWidget::showDock(bool show, int nr) {
+  _showDock = show;
+  setVisible(show);
+  auto *stackLayout = qobject_cast<QStackedLayout *>(layout());
+  stackLayout->setCurrentIndex(nr);
+  Q_EMIT visibilityChanged(show);
 }
 
-QToolBar* SidedockWidget::toolbar() const
-{
-    return _toolBar;
-}
+QToolBar *SidedockWidget::toolbar() const { return _toolBar; }
