b5840982019469c5ecf8d9a6f12efebb68f2f1fc
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2022-09-10 11:21:05 +0200

    Fix format

--- src/ui/sidedockwidget.cpp
+++ src/ui/sidedockwidget.cpp
@@ -20,149 +20,160 @@
 #include <QToolBar>
 #include <QWidget>
 
-SideToolButton::SideToolButton(QWidget *parent) : QToolButton(parent) {
-  setFocusPolicy(Qt::NoFocus);
-  KAcceleratorManager::setNoAccel(this);
-  setCheckable(true);
-  setAutoRaise(true);
-  setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
+SideToolButton::SideToolButton(QWidget *parent)
+    : QToolButton(parent)
+{
+    setFocusPolicy(Qt::NoFocus);
+    KAcceleratorManager::setNoAccel(this);
+    setCheckable(true);
+    setAutoRaise(true);
+    setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
 
-  wasChecked = false;
+    wasChecked = false;
 }
 
-Qt::Orientation SideToolButton::orientation() const { return Qt::Vertical; }
-
-QSize SideToolButton::sizeHint() const {
-  ensurePolished();
-
-  QStyleOptionToolButton option;
-  initStyleOption(&option);
-
-  QFontMetrics fm = fontMetrics();
-
-  // calculate example character width
-  const int charWidth = fm.boundingRect(QLatin1Char('x')).width();
-
-  // compute text size
-  QSize textSize = fm.size(Qt::TextShowMnemonic, option.text);
-  textSize.rwidth() += 2 * charWidth;
-
-  // compute icon size
-  const int spacing = 2;
-  const int iconwidth = option.iconSize.height();
-  const int iconheight = option.iconSize.width();
-
-  // compute final size. height and width are rotated.
-  return {4 + qMax(textSize.height(), iconheight) + spacing,
-          4 + textSize.width() + iconwidth + spacing};
+Qt::Orientation SideToolButton::orientation() const
+{
+    return Qt::Vertical;
 }
 
-void SideToolButton::paintEvent(QPaintEvent *event) {
-  Q_UNUSED(event)
+QSize SideToolButton::sizeHint() const
+{
+    ensurePolished();
 
-  // rotated paint
-  QStylePainter painter(this);
-  QStyleOptionToolButton option;
-  initStyleOption(&option);
+    QStyleOptionToolButton option;
+    initStyleOption(&option);
 
-  // first draw normal frame and not text/icon
-  option.text.clear();
-  option.icon = QIcon();
-  painter.drawComplexControl(QStyle::CC_ToolButton, option);
+    QFontMetrics fm = fontMetrics();
 
-  // rotate the options
-  QSize size(option.rect.size());
-  size.transpose();
-  option.rect.setSize(size);
+    // calculate example character width
+    const int charWidth = fm.boundingRect(QLatin1Char('x')).width();
 
-  // rotate the painter
-  painter.translate(width(), 0);
-  painter.rotate(90);
+    // compute text size
+    QSize textSize = fm.size(Qt::TextShowMnemonic, option.text);
+    textSize.rwidth() += 2 * charWidth;
 
-  // paint text and icon
-  option.text = text();
-  QIcon::Mode iconMode =
-      (option.state & QStyle::State_MouseOver) ? QIcon::Active : QIcon::Normal;
-  QPixmap ic = icon().pixmap(option.iconSize, iconMode, QIcon::On);
-  QTransform transform;
-  transform = transform.rotate(-90); // rotate right
-  option.icon = ic.transformed(transform, Qt::SmoothTransformation);
-  painter.drawControl(QStyle::CE_ToolButtonLabel, option);
-  painter.end();
+    // compute icon size
+    const int spacing = 2;
+    const int iconwidth = option.iconSize.height();
+    const int iconheight = option.iconSize.width();
+
+    // compute final size. height and width are rotated.
+    return {4 + qMax(textSize.height(), iconheight) + spacing, 4 + textSize.width() + iconwidth + spacing};
+}
+
+void SideToolButton::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event)
+
+    // rotated paint
+    QStylePainter painter(this);
+    QStyleOptionToolButton option;
+    initStyleOption(&option);
+
+    // first draw normal frame and not text/icon
+    option.text.clear();
+    option.icon = QIcon();
+    painter.drawComplexControl(QStyle::CC_ToolButton, option);
+
+    // rotate the options
+    QSize size(option.rect.size());
+    size.transpose();
+    option.rect.setSize(size);
+
+    // rotate the painter
+    painter.translate(width(), 0);
+    painter.rotate(90);
+
+    // paint text and icon
+    option.text = text();
+    QIcon::Mode iconMode = (option.state & QStyle::State_MouseOver) ? QIcon::Active : QIcon::Normal;
+    QPixmap ic = icon().pixmap(option.iconSize, iconMode, QIcon::On);
+    QTransform transform;
+    transform = transform.rotate(-90); // rotate right
+    option.icon = ic.transformed(transform, Qt::SmoothTransformation);
+    painter.drawControl(QStyle::CE_ToolButtonLabel, option);
+    painter.end();
 }
 
 SidedockWidget::SidedockWidget(QWidget *parent)
-    : QWidget(parent), _btnGroup(new QButtonGroup(this)), _showDock(false) {
-  Q_ASSERT(parent); // we need a parent widget
+    : QWidget(parent)
+    , _btnGroup(new QButtonGroup(this))
+    , _showDock(false)
+{
+    Q_ASSERT(parent); // we need a parent widget
 
-  _btnGroup->setExclusive(true);
+    _btnGroup->setExclusive(true);
 
-  _toolBar = new QToolBar(i18nc("@title", "Side Toolbar"), this);
-  _toolBar->setFloatable(false);
-  _toolBar->setMovable(false);
-  _toolBar->setObjectName("sidebar");
-  _toolBar->setIconSize(QSize(16, 16));
-  _toolBar->setContextMenuPolicy(Qt::PreventContextMenu);
-  setLayout(new QStackedLayout);
+    _toolBar = new QToolBar(i18nc("@title", "Side Toolbar"), this);
+    _toolBar->setFloatable(false);
+    _toolBar->setMovable(false);
+    _toolBar->setObjectName("sidebar");
+    _toolBar->setIconSize(QSize(16, 16));
+    _toolBar->setContextMenuPolicy(Qt::PreventContextMenu);
+    setLayout(new QStackedLayout);
 }
 
-void SidedockWidget::addDock(QWidget *widget, const QString &title,
-                             const QIcon &icon) {
-  widget->setVisible(false);
-  layout()->addWidget(widget);
+void SidedockWidget::addDock(QWidget *widget, const QString &title, const QIcon &icon)
+{
+    widget->setVisible(false);
+    layout()->addWidget(widget);
 
-  SideToolButton *button = new SideToolButton(this);
+    SideToolButton *button = new SideToolButton(this);
 
-  button->setText(title);
-  button->setToolTip(i18n("Toggle '%1' view.", title));
-  button->setIcon(icon);
-  button->setShortcut(QKeySequence());
-  button->setChecked(false); // initially do not check
-  button->wasChecked = false;
-  _btnGroup->addButton(button);
+    button->setText(title);
+    button->setToolTip(i18n("Toggle '%1' view.", title));
+    button->setIcon(icon);
+    button->setShortcut(QKeySequence());
+    button->setChecked(false); // initially do not check
+    button->wasChecked = false;
+    _btnGroup->addButton(button);
 
-  // only request action on user set action
-  const int idx = _widgets.count();
-  int idx_aux = idx;
+    // only request action on user set action
+    const int idx = _widgets.count();
+    int idx_aux = idx;
 
-  if (idx == 0) { // set widget of ElementTypes as standard
-    button->setChecked(true);
-    button->wasChecked = true;
-  }
+    if (idx == 0) { // set widget of ElementTypes as standard
+        button->setChecked(true);
+        button->wasChecked = true;
+    }
 
-  connect(button, &SideToolButton::clicked, this,
-          [this, button, idx, &idx_aux] {
-            // Force uncheck the button because of the exclusive
-            // Without this the toolbar buttons cannot be all unchecked
-            // after one is clicked
-            if (button->wasChecked) {
-              if (idx_aux == idx) {
+    connect(button, &SideToolButton::clicked, this, [this, button, idx, &idx_aux] {
+        // Force uncheck the button because of the exclusive
+        // Without this the toolbar buttons cannot be all unchecked
+        // after one is clicked
+        if (button->wasChecked) {
+            if (idx_aux == idx) {
                 _btnGroup->setExclusive(false);
                 button->setChecked(false);
                 _btnGroup->setExclusive(true);
                 button->wasChecked = false;
-              } else {
-                button->wasChecked = true;
-              }
             } else {
-              button->wasChecked = true;
+                button->wasChecked = true;
             }
+        } else {
+            button->wasChecked = true;
+        }
 
-            idx_aux = idx;
-            showDock(button->isChecked(), idx);
-          });
+        idx_aux = idx;
+        showDock(button->isChecked(), idx);
+    });
 
-  // register and add to list
-  _toolBar->addWidget(button);
-  _widgets.append(widget);
+    // register and add to list
+    _toolBar->addWidget(button);
+    _widgets.append(widget);
 }
 
-void SidedockWidget::showDock(bool show, int nr) {
-  _showDock = show;
-  setVisible(show);
-  auto *stackLayout = qobject_cast<QStackedLayout *>(layout());
-  stackLayout->setCurrentIndex(nr);
-  Q_EMIT visibilityChanged(show);
+void SidedockWidget::showDock(bool show, int nr)
+{
+    _showDock = show;
+    setVisible(show);
+    auto *stackLayout = qobject_cast<QStackedLayout *>(layout());
+    stackLayout->setCurrentIndex(nr);
+    Q_EMIT visibilityChanged(show);
 }
 
-QToolBar *SidedockWidget::toolbar() const { return _toolBar; }
+QToolBar *SidedockWidget::toolbar() const
+{
+    return _toolBar;
+}
